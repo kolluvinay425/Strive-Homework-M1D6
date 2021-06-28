8cdde8edf575131028d8ddab42fd80a46435b726
kolluvinay425 <kolluvinay425@gmail.com>
2021-06-28 16:39:09 +0200

    updated

--- day6.js
+++ day6.js
@@ -188,11 +188,11 @@ console.log('this is exercise 2 reesult:',whooIsBigger(15,20))
     Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
-splitMe = function(string){
-    return typeof sentence === 'string' && sentence.length > 0 ?
-    sentence.split(' ') : [];
-
+function splitMe(text) {
+  let splitedString = text.split(' ')
+  return splitedString
 }
+
 console.log('this is exercise 3 reesult:',splitMe('vinay kumar'))
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
